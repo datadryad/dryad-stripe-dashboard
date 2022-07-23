b086cac04bb4eec3a0219fa5836b1a02b7b1e951
Rishi <hrushikeshchapke@gmail.com>
2022-07-23 19:49:23 +0530

    manage_user role check for user management routes

--- src/api/routes/Auth.js
+++ src/api/routes/Auth.js
@@ -1,16 +1,20 @@
 import express from 'express';
 import { adminMiddleware, authMiddleware } from 'node-mongoose-auth/auth.js';
-import Invoice from '../mongo/Invoice.js';
-import { handleError } from '../stripe.js';
+import { handleError, notPermitted } from '../stripe.js';
 import { User } from "node-mongoose-auth";
 
 const router = express.Router();
 
 
 // router.post('/permit', adminMiddleware, async (req, res) => {
-router.post('/permit', async (req, res) => {
+router.post('/permit', authMiddleware, async (req, res) => {
 
     const data = req.body;
+    const user = req.user;
+
+    if(!(user.isAdmin || notPermitted(user, "general_permissions", "manage_users"))){
+        return handleError(res, "You are not authorized to manage Users.")
+    }
     console.log({
         $set : data.permission_updates
     });
@@ -30,9 +34,14 @@ router.post('/permit', async (req, res) => {
         }
     )
 })
-router.post('/withdraw', async (req, res) => {
+router.post('/withdraw', authMiddleware, async (req, res) => {
 
     const data = req.body;
+    const user = req.user;
+
+    if(!(user.isAdmin || notPermitted(user, "general_permissions", "manage_users"))){
+        return handleError(res, "You are not authorized to manage Users.")
+    }
     User.findOneAndUpdate(
         {
             _id : data.user_id
@@ -52,17 +61,25 @@ router.post('/withdraw', async (req, res) => {
     )
 })
 
-// router.post('/list', adminMiddleware, async (req, res) => {
-router.post('/list', async (req, res) => {
-    console.log(User);
-    let user;
-    try {
-        user = await Invoice.find();
-    } catch (error) {
-        user = error;
+
+router.post('/list', authMiddleware, async (req, res) => {
+
+    const data = req.body;
+    const user = req.user;
+
+    if(!(user.isAdmin || notPermitted(user, "general_permissions", "manage_users"))){
+        return handleError(res, "You are not authorized to manage Users.")
     }
 
-    return res.json(user);
+    User.find({}).limit(10).select("email isActive general_permissions invoice_permissions report_permissions").sort({createdAt : -1}).exec((err, resp) => {
+        if(err){
+            console.log(err);
+            return handleError(res, err);
+        }
+
+        return res.formatter.ok(resp);
+    })
+
 })
 
 // router.post('/delete', adminMiddleware, async (req, res) => {

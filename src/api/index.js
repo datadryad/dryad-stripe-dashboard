2036732f74d7d3d4f10b543096b27fdb97636a9f
Parshva Runwal <runwalparshva3@gmail.com>
2022-09-13 21:28:19 +0530

    Added patch library and imports fixed so as to start backend

--- src/api/index.js
+++ src/api/index.js
@@ -1,64 +1,56 @@
-import express from 'express';
-import path from "path";
-import {fileURLToPath} from 'url';
+import { default as dotenv } from "dotenv";
+import express from "express";
 import jwt from "jsonwebtoken";
-import {default as dotenv} from 'dotenv';
+import path from "path";
+import { fileURLToPath } from "url";
 dotenv.config();
 // require('dotenv').config();
-import cors from 'cors';
+import cors from "cors";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-
 
 // const mongo = require('@metamodules/mongo')().base;
 
 import invoiceRoutes from "./routes/Invoice.js";
 
-import reportRoutes from './routes/Report.js';
-import webHookRoutes from './routes/StripeWebhook.js';
-import { responseEnhancer } from 'express-response-formatter';
+import { responseEnhancer } from "express-response-formatter";
+import mongoose from "mongoose";
+import { authRouter } from "node-mongoose-auth";
+import { WebSocketServer } from "ws";
+import AuthRoutes from "./routes/Auth.js";
+import reportRoutes from "./routes/Report.js";
+import webHookRoutes from "./routes/StripeWebhook.js";
+import { initiateRestore, Stripe } from "./stripe.js";
+import { addActiveUserSession } from "./websocket_utils.js";
 
 process.env.SECRET_KEY = "DEV";
-import { authRouter } from 'node-mongoose-auth';
-import mongoose from 'mongoose';
-import AuthRoutes from './routes/Auth.js';
-import { initiateRestore, Stripe } from './stripe.js';
-import { createServer } from 'http';
-import { WebSocketServer } from 'ws';
-import { addActiveUserSession } from './websocket_utils.js';
 // const UserSchema = require("node-mongoose-auth/models/UserSchema").add({permissions : String});
 
-const app = express()
+const app = express();
 const port = 4000;
-app.use(cors())
+app.use(cors());
 
-const MONGO_URI = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_SERVICE_HOST}:${process.env.MONGO_SERVICE_PORT}/${process.env.MONGO_INITDB_DATABASE}?authSource=admin`
+const MONGO_URI = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_SERVICE_HOST}:${process.env.MONGO_SERVICE_PORT}/${process.env.MONGO_INITDB_DATABASE}?authSource=admin`;
 // const MONGO_URI = "mongodb+srv://admin:admin@cluster0.loydr.mongodb.net/mongo?retryWrites=true&w=majority"
 
 console.log(MONGO_URI);
 
-mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(r => {
-
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then((r) => {
   initiateRestore();
-  console.log("MongoDB Connected")
-  app.use('/auth', authRouter);
-  app.use('/users', AuthRoutes);
-
+  console.log("MongoDB Connected");
+  app.use("/auth", authRouter);
+  app.use("/users", AuthRoutes);
 });
 
-app.use('/webhook', webHookRoutes);
+app.use("/webhook", webHookRoutes);
 
 app.use(responseEnhancer());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.use('/invoices', invoiceRoutes);
-app.use('/reports', reportRoutes);
-
-
+app.use("/invoices", invoiceRoutes);
+app.use("/reports", reportRoutes);
 
 // PRINT ROUTES
 // function print (path, layer) {
@@ -94,35 +86,30 @@ app.use('/reports', reportRoutes);
 
 const server = app.listen(port, () => console.log(`Backend API listening on port ${port}!`));
 
-const wss = new WebSocketServer({noServer : true});
+const wss = new WebSocketServer({ noServer: true });
 
 global.sessions_mapping = {};
-wss.on('connection', async function connection(ws, request, user_id, report_id) {
+wss.on("connection", async function connection(ws, request, user_id, report_id) {
+  await addActiveUserSession(report_id, ws);
+  const reportRun = await Stripe.reporting.reportRuns.retrieve(report_id);
 
-  
-
-  await addActiveUserSession( report_id, ws);
-  const reportRun = await Stripe.reporting.reportRuns.retrieve( report_id );
-
-  if(reportRun.status !== "pending"){
+  if (reportRun.status !== "pending") {
     ws.send(JSON.stringify(reportRun));
     ws.close();
   }
 
-  ws.on('message', function message(data) {
+  ws.on("message", function message(data) {
     ws.send(data);
-
   });
 });
 
-server.on('upgrade', async function upgrade(request, socket, head) {      //handling upgrade(http to websocekt) event
-
+server.on("upgrade", async function upgrade(request, socket, head) {
+  //handling upgrade(http to websocekt) event
 
   //emit connection when request accepted
   wss.handleUpgrade(request, socket, head, async function done(ws) {
-
     try {
-      const search= request.url.substring(1);
+      const search = request.url.substring(1);
       const params = new URLSearchParams(search);
 
       const auth_token = params.get("auth_token");
@@ -131,14 +118,11 @@ server.on('upgrade', async function upgrade(request, socket, head) {      //hand
       // console.log(auth_token, report_id);
 
       const user_data = await jwt.verify(auth_token, process.env.SECRET_KEY);
-      
-      wss.emit('connection', ws, request, user_data.id, report_id);
+
+      wss.emit("connection", ws, request, user_data.id, report_id);
     } catch (error) {
       console.log(error.message || error);
       socket.destroy();
     }
   });
 });
-
-
-
--- src/api/routes/StripeWebhook.js
+++ src/api/routes/StripeWebhook.js
@@ -1,50 +1,45 @@
-import { ConsoleSqlOutlined } from '@ant-design/icons';
-import express from 'express';
-import stripe from 'stripe';
-import WebSocket, { WebSocketServer } from 'ws';
-import Invoice from '../mongo/Invoice.js';
-import { sendNotificationToAllActiveSessions } from '../Websocket_utils.js';
-
-
+import { ConsoleSqlOutlined } from "@ant-design/icons";
+import express from "express";
+import stripe from "stripe";
+import WebSocket, { WebSocketServer } from "ws";
+import Invoice from "../mongo/Invoice.js";
+import { sendNotificationToAllActiveSessions } from "../websocket_utils.js";
 
 const endpointSecret = "whsec_b0329a1720b7e2e47daa28b998870496263f2f172eeae0588cdf42d6ea7d1725";
 
 const router = express.Router();
 
-router.post('/updates', express.raw({type: 'application/json'}), async (req, res) => {
-    
-    const signature = req.headers['stripe-signature'];
+router.post("/updates", express.raw({ type: "application/json" }), async (req, res) => {
+  const signature = req.headers["stripe-signature"];
 
-    let event;
+  let event;
 
-    try {
-        event = stripe.webhooks.constructEvent(req.body, signature, endpointSecret);
-        
-        if(event.type == "invoice.updated"){
-            console.log("Updating invoice : " + event.data.object.id)
-            Invoice.updateOne(
-                {_id : event.data.object.id},
-                event.data.object,
-                {
-                    new : true
-                },
-                (err, res) => {
-                    console.log(err, res);
-                }
-            )
+  try {
+    event = stripe.webhooks.constructEvent(req.body, signature, endpointSecret);
+
+    if (event.type == "invoice.updated") {
+      console.log("Updating invoice : " + event.data.object.id);
+      Invoice.updateOne(
+        { _id: event.data.object.id },
+        event.data.object,
+        {
+          new: true,
+        },
+        (err, res) => {
+          console.log(err, res);
         }
-        if(event.type === "reporting.report_run.succeeded"){
-            await sendNotificationToAllActiveSessions(event.data.object.id, event.data.object);
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(400).send(`Webhook Error: ${error.message}`);
-        return;
+      );
     }
+    if (event.type === "reporting.report_run.succeeded") {
+      await sendNotificationToAllActiveSessions(event.data.object.id, event.data.object);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(`Webhook Error: ${error.message}`);
+    return;
+  }
 
-    res.send();
-
-})
-
+  res.send();
+});
 
 export default router;
